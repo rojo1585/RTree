9f72af203f179f2fa89cc0d0af7a7d64ac52e420
jorge.mendoza <jorge.mendoza@exodus.com.mx>
2025-11-18 19:36:57 -0600

    Feat: BTree Implementation

---
+++ src/Implementations/BTree.cs
@@ -0,0 +1,93 @@
+﻿using RTree.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTree.Implementations;
+
+public class BTree<T> : ITree<T> where T : IComparable<T>
+{
+    private BTreeNode<T>? _root;
+    private readonly int _order;
+
+    private int MaxKeys => _order - 1;
+    private int MinKeys => (int)Math.Ceiling((double)_order / 2) - 1;
+    public BTree(int order = 5)
+    {
+        if (order < 3)
+            throw new ArgumentOutOfRangeException(nameof(order), "The order of the B-Tree must be greater than or equal to 3.");
+
+        _order = order;
+
+        _root = new BTreeNode<T> { IsLeaf = true };
+    }
+
+    public void Insert(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T value)
+    {
+        return Search(_root, value);
+    }
+
+    private bool Search(BTreeNode<T>? node, T value)
+    {
+        if (node == null)
+            return false;
+
+        int index = node.FindKeyIndex(value);
+
+        if (index >= 0)
+        {
+            return true;
+        }
+
+        if (!node.IsLeaf)
+        {
+            // The branch index to follow is ~index (bitwise complement)
+            int childIndex = ~index;
+            return Search(node.Children[childIndex], value);
+        }
+
+        return false; 
+    }
+
+    public void Delete(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> TraverseInOrder()
+    {
+        return TraverseInOrderRec(_root);
+    }
+
+    private IEnumerable<T> TraverseInOrderRec(BTreeNode<T>? node)
+    {
+        if (node == null) yield break;
+
+        for (int i = 0; i < node.Keys.Count; i++)
+        {
+            if (!node.IsLeaf)
+            {
+                foreach (var val in TraverseInOrderRec(node.Children[i]))
+                    yield return val;
+            }
+
+            yield return node.Keys[i];
+        }
+
+        if (!node.IsLeaf)
+        {
+            foreach (var val in TraverseInOrderRec(node.Children[node.Keys.Count]))
+                yield return val;
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator() => TraverseInOrder().GetEnumerator();
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ src/Implementations/BTreeNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTree.Implementations;
+/// <summary>
+/// Represents a node in the B-Tree.
+/// </summary>
+internal class BTreeNode<T> where T : IComparable<T>
+{
+
+    public List<T> Keys { get; set; } = [];
+    public List<BTreeNode<T>?> Children { get; set; } = [];
+    public bool IsLeaf { get; set; } = true;
+
+    /// <summary>
+    /// Utiliza búsqueda binaria para encontrar la posición de una clave dada.
+    /// </summary>
+    /// <param name="value">La clave a buscar.</param>
+    /// <returns>
+    /// Si la clave se encuentra, retorna su índice (>= 0).
+    /// Si la clave NO se encuentra, retorna el índice donde debería insertarse, representado como el complemento bit a bit (~índice). 
+    /// Este valor también indica el índice del puntero al nodo hijo que debemos seguir.
+    /// </returns>
+    public int FindKeyIndex(T value)
+    {
+        int index = Keys.BinarySearch(value);
+
+        if (index >= 0)
+        {
+            return index;
+        }
+
+        return ~index;
+    }
+}
