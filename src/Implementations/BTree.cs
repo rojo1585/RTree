b4efaea07d6d38dbfd75566ac90d42d1b9d73aad
jorge.mendoza <jorge.mendoza@exodus.com.mx>
2025-12-11 22:14:43 -0600

    feat: Implement delete

--- src/Implementations/BTree.cs
+++ src/Implementations/BTree.cs
@@ -1,9 +1,4 @@
 ﻿using RTree.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RTree.Implementations;
 
@@ -37,7 +32,7 @@ public class BTree<T> : ITree<T> where T : IComparable<T>
             SplitChild(s, 0);
 
             InsertNonFull(s, value);
-    }
+        }
         else
         {
             InsertNonFull(r, value);
@@ -57,23 +52,31 @@ public class BTree<T> : ITree<T> where T : IComparable<T>
         int index = node.FindKeyIndex(value);
 
         if (index >= 0)
-        {
             return true;
-        }
 
         if (!node.IsLeaf)
         {
-            // The branch index to follow is ~index (bitwise complement)
             int childIndex = ~index;
             return Search(node.Children[childIndex], value);
         }
 
-        return false; 
+        return false;
     }
 
     public void Delete(T value)
     {
-        throw new NotImplementedException();
+        if (_root == null) return;
+
+        DeleteRec(_root, value);
+
+        if (_root.Keys.Count == 0)
+        {
+            if (_root.IsLeaf)
+                _root = null;
+            else
+                _root = _root.Children.FirstOrDefault();
+
+        }
     }
 
     public IEnumerable<T> TraverseInOrder()
@@ -159,4 +162,137 @@ public class BTree<T> : ITree<T> where T : IComparable<T>
     }
     public IEnumerator<T> GetEnumerator() => TraverseInOrder().GetEnumerator();
     System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private T GetPredecessor(BTreeNode<T> node, int index)
+    {
+        BTreeNode<T> current = node.Children[index]!;
+
+        while (!current.IsLeaf)
+            current = current.Children[current.Keys.Count]!;
+
+        return current.Keys.Last();
+    }
+
+    private T GetSuccessor(BTreeNode<T> node, int index)
+    {
+        BTreeNode<T> current = node.Children[index + 1]!;
+
+        while (!current.IsLeaf)
+            current = current.Children[0]!;
+
+        return current.Keys.First();
+    }
+
+    private void BorrowFromPrevious(BTreeNode<T> node, int index)
+    {
+        BTreeNode<T> child = node.Children[index]!;
+        BTreeNode<T> sibling = node.Children[index - 1]!;
+
+        child.Keys.Insert(0, node.Keys[index - 1]);
+
+        node.Keys[index - 1] = sibling.Keys.Last();
+
+        if (!child.IsLeaf)
+        {
+            child.Children.Insert(0, sibling.Children.Last());
+            sibling.Children.RemoveAt(sibling.Children.Count - 1);
+        }
+
+        sibling.Keys.RemoveAt(sibling.Keys.Count - 1);
+    }
+
+    private void BorrowFromNext(BTreeNode<T> node, int index)
+    {
+        BTreeNode<T> child = node.Children[index]!;
+        BTreeNode<T> sibling = node.Children[index + 1]!;
+
+        child.Keys.Add(node.Keys[index]);
+
+        node.Keys[index] = sibling.Keys.First();
+
+        if (!child.IsLeaf)
+        {
+            child.Children.Add(sibling.Children.First());
+            sibling.Children.RemoveAt(0);
+        }
+
+        sibling.Keys.RemoveAt(0);
+    }
+
+    private void Merge(BTreeNode<T> node, int index)
+    {
+        BTreeNode<T> child = node.Children[index]!;
+        BTreeNode<T> sibling = node.Children[index + 1]!;
+
+        child.Keys.Add(node.Keys[index]);
+
+        child.Keys.AddRange(sibling.Keys);
+
+        if (!child.IsLeaf)
+            child.Children.AddRange(sibling.Children);
+
+        node.Keys.RemoveAt(index);
+
+        node.Children.RemoveAt(index + 1);
+    }
+
+    private void Fill(BTreeNode<T> node, int index)
+    {
+        if (index != 0 && node.Children[index - 1]!.Keys.Count > MinKeys)
+            BorrowFromPrevious(node, index);
+        else if (index != node.Keys.Count && node.Children[index + 1]!.Keys.Count > MinKeys)
+            BorrowFromNext(node, index);
+        else
+        {
+            if (index != node.Keys.Count)
+                Merge(node, index);
+            else
+                Merge(node, index - 1);
+        }
+    }
+
+    private void DeleteRec(BTreeNode<T> node, T value)
+    {
+        int index = node.FindKeyIndex(value);
+
+        if (index >= 0)
+        {
+            if (node.IsLeaf)
+                node.Keys.RemoveAt(index);
+            else
+            {
+                if (node.Children[index]!.Keys.Count > MinKeys)
+                {
+                    T pred = GetPredecessor(node, index);
+                    node.Keys[index] = pred;
+                    DeleteRec(node.Children[index]!, pred);
+                }
+                else if (node.Children[index + 1]!.Keys.Count > MinKeys)
+                {
+                    T succ = GetSuccessor(node, index);
+                    node.Keys[index] = succ;
+                    DeleteRec(node.Children[index + 1]!, succ);
+                }
+                else
+                {
+                    Merge(node, index);
+                    DeleteRec(node.Children[index]!, value);
+                }
+            }
+        }
+        else
+        {
+            int childIndex = ~index;
+
+            if (node.IsLeaf)
+                return;
+
+            if (node.Children[childIndex]!.Keys.Count == MinKeys)
+                Fill(node, childIndex);
+
+            index = node.FindKeyIndex(value);
+            childIndex = ~index;
+            DeleteRec(node.Children[childIndex]!, value);
+        }
+    }
 }
