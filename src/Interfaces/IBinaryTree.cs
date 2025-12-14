7e08d7181908536ebb9b04f61c2bdd6a189adbc3
jorge.mendoza <jorge.mendoza@exodus.com.mx>
2025-12-14 00:58:51 -0600

    fix

--- src/Interfaces/IBinaryTree.cs
+++ src/Interfaces/IBinaryTree.cs
@@ -2,6 +2,8 @@
 
 public interface IBinaryTree<T> : ITree<T> where T : IComparable<T>
 {
+    void Insert(T value);
+    void Delete(T value);
     IEnumerable<T> TraversePreOrder();
     IEnumerable<T> TraversePostOrder();
 }
